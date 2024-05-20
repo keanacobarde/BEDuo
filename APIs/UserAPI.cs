16f8be84183c40e92eeced7dfbf387ed6b843206
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 10:07:04 -0500

    Created UserAPI calls

--- APIs/UserAPI.cs
+++ APIs/UserAPI.cs
@@ -1,9 +1,35 @@
-﻿namespace BEDuo.APIs
+﻿using BEDuo.DTO;
+using BEDuo.Models;
+
+namespace BEDuo.APIs
 {
     public class UserAPI
     {
         public static void Map(WebApplication app)
         {
+            app.MapPost("/checkuser", (BEDuoDbContext db, UserAuthDto userAuthDto) => 
+            {
+                var userUid = db.Users.SingleOrDefault(user => user.Uid == userAuthDto.Uid);
+
+                if (userUid == null)
+                {
+                    return Results.NotFound();
+                }
+                else
+                {
+                    return Results.Ok(userUid);
+                }
+            });
+
+            app.MapGet("/users/{id}", (BEDuoDbContext db, int id) => {
+                User user = db.Users.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(user);
+            });
+
         }
     }
 }
