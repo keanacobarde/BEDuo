87eeff33866c139a68d713dc30ce72f12cc09e2a
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 13:07:45 -0500

    Adding edit API call to classes

--- APIs/ClassesAPI.cs
+++ APIs/ClassesAPI.cs
@@ -28,6 +28,63 @@ namespace BEDuo.APIs
                 return Results.Ok(db.Classes);
             });
 
+            app.MapPatch("/classes/{id}", (BEDuoDbContext db, int classId, Classes editedClass) => 
+            {
+                try
+                {
+                    Classes classToEdit = db.Classes.FirstOrDefault(c => c.Id == classId);
+                    if (classToEdit == null)
+                    {
+                        return Results.NotFound();
+                    }
+
+                    if (editedClass.Id != 0)
+                    {
+                        classToEdit.Id = editedClass.Id;
+                    };
+
+                    if (editedClass.Name != null)
+                    {
+                        classToEdit.Name = editedClass.Name;
+                    };
+
+                    if (editedClass.Description != null)
+                    {
+                        classToEdit.Description = editedClass.Description;
+                    };
+
+                    if (editedClass.University != null)
+                    {
+                        classToEdit.University = editedClass.University;
+                    };
+
+                    if (editedClass.StartDate != null)
+                    {
+                        classToEdit.StartDate = editedClass.StartDate;
+                    };
+
+                    if (editedClass.EndDate != null)
+                    {
+                        classToEdit.EndDate = editedClass.EndDate;
+                    };
+
+                    if (editedClass.isPublic != null)
+                    {
+                        classToEdit.isPublic = editedClass.isPublic;
+                    };
+
+                    if (editedClass.UserId != null)
+                    {
+                        classToEdit.UserId = editedClass.UserId;
+                    };
+
+                }
+                catch
+                {
+                    return Results.NotFound();
+                }
+            });
+
             app.MapDelete("/classes/{classId}", (BEDuoDbContext db, int classId) => 
             {
                 Classes classToDelete = db.Classes.FirstOrDefault(c => c.Id == classId);
@@ -39,6 +96,7 @@ namespace BEDuo.APIs
                 db.SaveChanges();
                 return Results.Ok(db.Classes);
             });
+
         }
     }
 }
