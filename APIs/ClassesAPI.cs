a966fe4338fcf64f57b3e732d1cdac0905df3613
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 12:55:51 -0500

    Adding class APIs

--- APIs/ClassesAPI.cs
+++ APIs/ClassesAPI.cs
@@ -1,9 +1,44 @@
-﻿namespace BEDuo.APIs
+﻿using BEDuo.Models;
+
+namespace BEDuo.APIs
 {
     public class ClassesAPI
     {
         public static void Map(WebApplication app)
         {
+            app.MapGet("/classes/{user}", (BEDuoDbContext db, int userId) =>
+            {
+                try
+                {
+                    var allClassesAssociatedWithUser = from classes in db.Classes
+                                                         where classes.UserId == userId
+                                                         select classes;
+                    return Results.Ok(allClassesAssociatedWithUser);
+                }
+                catch
+                {
+                    return Results.NotFound();
+                }
+            });
+
+            app.MapPost("/classes", (BEDuoDbContext db, Classes classToAdd) => 
+            { 
+                db.Classes.Add(classToAdd);
+                db.SaveChanges();
+                return Results.Ok(db.Classes);
+            });
+
+            app.MapDelete("/classes/{classId}", (BEDuoDbContext db, int classId) => 
+            {
+                Classes classToDelete = db.Classes.FirstOrDefault(c => c.Id == classId);
+                if (classToDelete == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Classes.Remove(classToDelete);
+                db.SaveChanges();
+                return Results.Ok(db.Classes);
+            });
         }
     }
 }
