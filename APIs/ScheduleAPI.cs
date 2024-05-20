8991c58f8671f58367b89647bd4e9d5cc010f5e3
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 12:44:10 -0500

    Added API call to filter schedules by user and display all schedules that belong to a given user

--- APIs/ScheduleAPI.cs
+++ APIs/ScheduleAPI.cs
@@ -1,9 +1,25 @@
-﻿namespace BEDuo.APIs
+﻿using BEDuo.Models;
+
+namespace BEDuo.APIs
 {
     public class ScheduleAPI
     {
         public static void Map(WebApplication app)
         {
+            app.MapGet("/schedule/{user}", (BEDuoDbContext db, int userId) =>
+            {
+                try
+                {
+                    var allSchedulesAssociatedWithUser = from schedules in db.Schedules
+                                                         where schedules.UserID == userId
+                                                         select schedules;
+                    return Results.Ok(allSchedulesAssociatedWithUser);
+                }
+                catch
+                {
+                    return Results.NotFound();
+                }
+            });
         }
     }
 }
