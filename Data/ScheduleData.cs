69831ec78c31894dff7e3fb9cbb0e25728214482
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 09:28:36 -0500

    Adding schedule test data

--- Data/ScheduleData.cs
+++ Data/ScheduleData.cs
@@ -6,6 +6,33 @@ namespace BEDuo.Data
     {
         public static List<Schedule> Schedules = new List<Schedule>
         {
+            new Schedule()
+            {
+                Id=1,
+                Title="A.P. Exams",
+                StartDate= new DateTime(2023, 8, 2),
+                EndDate= new DateTime(2024, 9, 2),
+                UserID= 1,
+                IsPublic= true
+            },
+            new Schedule()
+            {
+                Id=2,
+                Title="Programming",
+                StartDate= new DateTime(2023, 8, 2),
+                EndDate= new DateTime(2024, 9, 2),
+                UserID= 1,
+                IsPublic= false
+            },
+            new Schedule()
+            {
+                Id=3,
+                Title="Music",
+                StartDate= new DateTime(2023, 10, 2),
+                EndDate= new DateTime(2024, 9, 2),
+                UserID= 2,
+                IsPublic= true
+            }
         };
     }
 }
