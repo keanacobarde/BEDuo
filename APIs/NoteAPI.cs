45022c307c1e41172fc67abd0e4d2a74a5e1d799
Keana Cobarde <keanacobarde@gmail.com>
2024-05-20 18:27:35 -0500

    Adding preliminary editing functionality for ntoes as well as other API calls

--- APIs/NoteAPI.cs
+++ APIs/NoteAPI.cs
@@ -6,6 +6,63 @@ namespace BEDuo.APIs
     {
         public static void Map(WebApplication app)
         {
+            app.MapGet("/notes/{classId}", (BEDuoDbContext db, int classId) => 
+            { 
+                var classNotes = from notes in db.Notes
+                                 where notes.ClassId == classId
+                                 select notes;
+                if (classNotes == null)
+                { 
+                    return Results.NotFound();
+                }
+                return Results.Ok(classNotes);
+            });
+
+            app.MapPost("/notes", (BEDuoDbContext db, Notes notesToAdd) => 
+            {
+                try
+                {
+                    db.Notes.Add(notesToAdd);
+                    db.SaveChanges();
+                    return Results.Ok(db.Notes.Where(n => n.ClassId == notesToAdd.ClassId));
+                }
+                catch { 
+                    return Results.BadRequest();
+                }
+            });
+
+            app.MapPatch("notes/{id}", (BEDuoDbContext db, int id, Notes editedNote) =>
+            {
+                try
+                {
+                    var noteToEdit = db.Notes.FirstOrDefault(n => n.ClassId == id);
+                    if (noteToEdit == null)
+                    {
+                        return Results.BadRequest();
+                    }
+
+                    if (editedNote.ClassId != default)
+                    {
+                        noteToEdit.ClassId = editedNote.ClassId;
+                    }
+
+                    if (editedNote.DateCreated != default)
+                    {
+                        noteToEdit.DateCreated = editedNote.DateCreated;
+                    }
+
+                    if (editedNote.Description != null)
+                    {
+                        noteToEdit.Description = editedNote.Description; 
+                    }
+
+                    return Results.Ok(noteToEdit);
+                }
+                catch {
+                    return Results.BadRequest();
+                }
+            });
+
             app.MapDelete("/notes/{noteId}", (BEDuoDbContext db, int noteId) =>
             {
                 Notes noteToDelete = db.Notes.FirstOrDefault(c => c.Id == noteId);
